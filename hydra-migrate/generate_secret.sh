fc4fee51d9f0f43dc0cb908073c163571c4e854e
Ahmet AYGÜN <me@ahmetaygun.net>
2018-12-15 16:04:41 +0300

    updated hydra-migrate
    - removed system secret from configmap
    - store system secret as stringData in k8s secrets
    - generate system secret as 32 chars
    - fix image to oryOS.10

--- hydra-migrate/generate_secret.sh
+++ hydra-migrate/generate_secret.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-secret=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 64 | base64)
+secret=$(LC_ALL=C tr -dc 'A-Za-z0-9' < /dev/urandom | base64 | head -c 32)
 
 cat > secret.yaml <<EOF
 apiVersion: v1
@@ -8,6 +8,6 @@ kind: Secret
 metadata:
   name: hydra-system-secret
 type: Opaque
-data:
-  hydra_system_secret: $secret
+stringData:
+  HYDRA_SYSTEM_SECRET: $secret
 EOF
