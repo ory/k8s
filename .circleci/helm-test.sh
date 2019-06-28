f8d3011b6090f7427be2e5aea362fab534f2f8af
aeneasr <aeneas@ory.sh>
2019-06-28 13:25:08 +0200

    u

--- .circleci/helm-test.sh
+++ .circleci/helm-test.sh
@@ -3,6 +3,8 @@
 set -Eeuxo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-helm install --tiller-namespace helm-ci --namespace helm-ci -f .circleci/values/oathkeeper.yaml -n cci-$1-${CIRCLE_SHA1} ./helm/charts/$1
-helm test --tiller-namespace helm-ci cci-$1-${CIRCLE_SHA1}
-helm del --tiller-namespace helm-ci --purge cci-$1-${CIRCLE_SHA1}
+release=$(echo cci-$1-${CIRCLE_SHA1}| cut -c 1-50)
+
+helm install --tiller-namespace helm-ci --namespace helm-ci -f .circleci/values/oathkeeper.yaml -n ${release} ./helm/charts/$1
+helm test --tiller-namespace helm-ci ${release}
+helm del --tiller-namespace helm-ci --purge ${release}
