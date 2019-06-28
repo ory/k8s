09b61e50f2c6554d84f985ceaa84b2f1bb4da1b3
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-06-28 14:17:15 +0200

    Add helm test to CI pipeline (#17)

---
+++ .circleci/helm-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -Eeuxo pipefail
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
+release=$(echo cci-$1-${CIRCLE_SHA1}| cut -c 1-50)
+
+function cleanup {
+    helm del --tiller-namespace helm-ci --purge ${release}
+}
+trap cleanup EXIT
+
+helm install --tiller-namespace helm-ci --namespace helm-ci -f .circleci/values/$1.yaml -n ${release} ./helm/charts/$1
+
+n=0
+until [[ $n -ge 5 ]]; do
+  helm test --timeout 90 --cleanup --tiller-namespace helm-ci ${release} && exit 0  # substitute your command here
+  kubectl delete pods/${release}-test-connection --namespace helm-ci
+  n=$[$n+1]
+  sleep 5
+done
+
+exit 1
