ab2788628254516e2dfb3d20bd33f70a873da39a
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-07-11 22:14:10 +0200

    ci: Set namespace and service account from env (#26)

--- .circleci/helm-test.sh
+++ .circleci/helm-test.sh
@@ -6,16 +6,16 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 release=$(echo cci-$(date +%s)-$1-${CIRCLE_SHA1}| cut -c 1-50)
 
 function cleanup {
-    helm del --tiller-namespace helm-ci --purge ${release} || true
+    helm del --tiller-namespace ${TILLER_NAMESPACE} --purge ${release} || true
 }
 trap cleanup EXIT
 
-helm install --tiller-namespace helm-ci --namespace helm-ci -f .circleci/values/$1.yaml -n ${release} ./helm/charts/$1
+helm install --tiller-namespace ${TILLER_NAMESPACE} --namespace ${TILLER_NAMESPACE} -f .circleci/values/$1.yaml -n ${release} ./helm/charts/$1
 
 n=0
 until [[ $n -ge 5 ]]; do
-  helm test --timeout 90 --cleanup --tiller-namespace helm-ci ${release} && exit 0  # substitute your command here
-  kubectl delete pods/${release}-test-connection --namespace helm-ci
+  helm test --timeout 90 --cleanup --tiller-namespace ${TILLER_NAMESPACE} ${release} && exit 0  # substitute your command here
+  kubectl delete pods/${release}-test-connection --namespace ${TILLER_NAMESPACE}
   n=$[$n+1]
   sleep 5
 done
