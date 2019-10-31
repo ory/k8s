ceb211342dcbb4709f05598d3cc056cd89087173
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-10-31 14:24:37 +0100

    Update helm-test.sh

--- .circleci/helm-test.sh
+++ .circleci/helm-test.sh
@@ -6,6 +6,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 release=$(echo cci-$(date +%s)-$1-${CIRCLE_SHA1}| cut -c 1-20)
 
 function cleanup {
+    kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
+    kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
     helm del --purge ${release} || true
 }
 trap cleanup EXIT
