8404ec377a32efd6c20f2b42bebda0dea6f82684
aeneasr <aeneas@ory.sh>
2020-04-22 14:19:38 +0200

    fix: adapt to kind breaking changes

--- .circleci/helm-test.sh
+++ .circleci/helm-test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 set -Eeuxo pipefail
+
+cfg=$(mktemp)
+kind get kubeconfig > $cfg
+export KUBECONFIG="$cfg"
+
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 release=$(echo cci-$(date +%s)-$1-${CIRCLE_SHA1} | cut -c 1-15)r
 
 function cleanup {
-    kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
-    kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
+    # kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
+    # kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
     helm del ${release} || true
+    exit 0
 }
 trap cleanup EXIT
 
@@ -16,8 +22,8 @@ helm dep update ./helm/charts/$1
 helm install -f .circleci/values/$1.yaml ${release} ./helm/charts/$1 --wait --debug
 
 n=0
-until [[ $n -ge 5 ]]; do
-  helm test --timeout 90 --cleanup ${release} && exit 0  # substitute your command here
+until [[ $n -ge 15 ]]; do
+  helm test --timeout 90s --cleanup ${release} && exit 0  # substitute your command here
   kubectl delete pod ${release}-test-connection
   n=$[$n+1]
   sleep 10
