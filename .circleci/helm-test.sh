3681891d52af58bb94e474dc6ae7394fe0cb7fc9
Demonsthere <daemonsthere@gmail.com>
2021-06-25 09:36:04 +0200

    chore: shellcheck

--- .circleci/helm-test.sh
+++ .circleci/helm-test.sh
@@ -2,21 +2,21 @@
 
 set -Eeuxo pipefail
 
-export cfg=$(mktemp)
+cfg=$(mktemp)
+export cfg
 export KUBECONFIG="$cfg"
 export TIMEOUT="30s"
 
-kind get kubeconfig > $cfg
-
+kind get kubeconfig > "$cfg"
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-release=$(echo cci-$(date +%s)-$1-${CIRCLE_SHA1} | cut -c 1-15)r
+release=$(echo "cci-$(date +%s)-$1-${CIRCLE_SHA1}" | cut -c 1-15)r
 
-helm dep update ./helm/charts/$1
+helm dep update "./helm/charts/$1"
 
 set +e
-helm install -f .circleci/values/$1.yaml ${release} ./helm/charts/$1 --wait --debug --timeout="${TIMEOUT}"
+helm install -f ".circleci/values/$1.yaml" "${release}" "./helm/charts/$1" --wait --debug --timeout="${TIMEOUT}"
 export INSTALLATION_STATUS=$?
 set -e
 
@@ -29,13 +29,13 @@ fi
 
 n=0
 until [[ $n -ge 15 ]]; do
-  helm test --timeout "${TIMEOUT}" ${release} && TEST_STATUS=$? && exit 0  # substitute your command here
-  kubectl delete pod ${release}-test-connection
+  helm test --timeout "${TIMEOUT}" "${release}" && TEST_STATUS=$? && exit 0  # substitute your command here
+  kubectl delete pod "${release}-test-connection"
   if [[ ${TEST_STATUS} -eq 0 ]]; then
     echo "Test Successful"
     exit ${TEST_STATUS}
   fi
-  n=$[$n+1]
+  n=$(( n+1 ))
   sleep 10
 done
 
