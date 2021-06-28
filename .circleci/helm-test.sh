a84bcec65bd8cb421b9c04975041822693866636
Jakub Błaszczyk <daemonsthere@gmail.com>
2021-06-28 12:06:53 +0200

    ci: run pipeline tests and fail on issues (#288)

--- .circleci/helm-test.sh
+++ .circleci/helm-test.sh
@@ -3,30 +3,44 @@
 set -Eeuxo pipefail
 
 cfg=$(mktemp)
-kind get kubeconfig > $cfg
+export cfg
 export KUBECONFIG="$cfg"
+export TIMEOUT="120s"
+
+kind get kubeconfig > "$cfg"
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-release=$(echo cci-$(date +%s)-$1-${CIRCLE_SHA1} | cut -c 1-15)r
+release=$(echo "cci-$(date +%s)-$1-${CIRCLE_SHA1}" | cut -c 1-15)r
 
-function cleanup {
-    # kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
-    # kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
-    helm del ${release} || true
-    exit 0
-}
-trap cleanup EXIT
+helm dep update "./helm/charts/$1"
 
-helm dep update ./helm/charts/$1
-helm install -f .circleci/values/$1.yaml ${release} ./helm/charts/$1 --wait --debug
+set +e
+helm install -f ".circleci/values/$1.yaml" "${release}" "./helm/charts/$1" --wait --debug --timeout="${TIMEOUT}"
+export INSTALLATION_STATUS=$?
+set -e
+
+if [[ ${INSTALLATION_STATUS} -ne 0 ]]; then
+  echo "Installation failed"
+  kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
+  kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
+  exit "${INSTALLATION_STATUS}"
+fi
 
 n=0
 until [[ $n -ge 15 ]]; do
-  helm test --timeout 90s --cleanup ${release} && exit 0  # substitute your command here
-  kubectl delete pod ${release}-test-connection
-  n=$[$n+1]
+  set +e
+  helm test --timeout "${TIMEOUT}" "${release}"
+  TEST_STATUS=$?
+  set -e
+
+  if [[ ${TEST_STATUS} -eq 0 ]]; then
+    echo "Test Successful"
+    exit ${TEST_STATUS}
+  fi
+  n=$(( n+1 ))
   sleep 10
 done
 
+echo "Something failed along the way"
 exit 1
