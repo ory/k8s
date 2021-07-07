1b0b8fe42b9e40a2198b51005c3dfcbbeb1aa556
Demonsthere <daemonsthere@gmail.com>
2021-07-07 13:45:46 +0200

    feat: add watcher pilot

---
+++ helm/charts/keto/files/watch.sh
@@ -0,0 +1,14 @@
+set -e
+set -u
+
+function rollOut() {
+  DEPLOY=$(kubectl get deploy -n "${NAMESPACE}" -l "${1}" -o name)
+  kubectl set env -n $NAMESPACE ${DEPLOY} sync=$(date "+%Y%m%d-%H%M%S")
+  kubectl rollout status -n $NAMESPACE ${DEPLOY}
+}
+
+inotifywait -e DELETE_SELF -m "${WATCH_FILE}" |
+   while read path _ file; do
+       echo "---> $path$file modified"
+       rollOut "${DEPLOYMENT_SELECTOR}"
+   done
