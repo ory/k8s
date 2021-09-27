0aed15cdec4d5bd6a48c9ba65473f20ced4420b5
Jakub Błaszczyk <daemonsthere@gmail.com>
2021-09-27 17:06:17 +0200

    feat: Introduce upgrade pipeline (#333)
    * sre: Introduce upgrade pipeline
    
    * remove separator
    
    * u
    
    * what is wrong with you
    
    * make upgrade less verbose
    
    * make upgrade less verbose

--- .circleci/helm-upgrade.sh
+++ .circleci/helm-upgrade.sh
@@ -13,12 +13,10 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 release=$(echo "cci-$(date +%s)-$1-${CIRCLE_SHA1}" | cut -c 1-15)r
 
-helm dep update "./helm/charts/$1"
-
 echo "---> Installing $1"
 
 set +e
-helm install -f ".circleci/values/$1.yaml" "${release}" "./helm/charts/$1" --wait --debug --timeout="${TIMEOUT}"
+helm install -f ".circleci/values/$1.yaml" "${release}" "ory/$1" --wait --timeout="${TIMEOUT}"
 export INSTALLATION_STATUS=$?
 set -e
 
@@ -27,6 +25,20 @@ if [[ ${INSTALLATION_STATUS} -ne 0 ]]; then
   kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
   kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
   exit "${INSTALLATION_STATUS}"
+fi
+
+echo "---> Upgrading $1"
+
+set +e
+helm upgrade -f ".circleci/values/$1.yaml" "${release}" "./helm/charts/$1" --wait --debug --timeout="${TIMEOUT}"
+export UPGRADE_STATUS=$?
+set -e
+
+if [[ ${UPGRADE_STATUS} -ne 0 ]]; then
+  echo "Upgrade of ${1} failed"
+  kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
+  kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
+  exit "${UPGRADE_STATUS}"
 fi
 
 echo "---> Testing $1"
