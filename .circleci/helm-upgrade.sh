700161fa7f4dbee222a932c19bf79ab66cc47fbc
Demonsthere <daemonsthere@gmail.com>
2022-03-22 14:02:46 +0100

    fix: upgrade ci

--- .circleci/helm-upgrade.sh
+++ .circleci/helm-upgrade.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set -Eeuo pipefail
-
+set -x
 cfg=$(mktemp)
 export cfg
 export KUBECONFIG="$cfg"
 export TIMEOUT="120s"
-export BASE_BRANCH="master"
+export BASE_RELEASE=$(curl -s https://api.github.com/repos/ory/k8s/releases/latest | grep "tag_name" | cut -d ':' -f 2 | tr -d '", ')
 
 kind get kubeconfig > "$cfg"
 
@@ -20,10 +20,10 @@ function teardown() {
 
 trap teardown HUP INT QUIT TERM EXIT
 
-echo "---> Installing $1 from ${BASE_BRANCH}"
+echo "---> Installing $1 from ${BASE_RELEASE}"
 
 set +e
-helm install -f "https://raw.githubusercontent.com/ory/k8s/${BASE_BRANCH}/.circleci/values/$1.yaml" "${release}" "ory/$1" --wait --timeout="${TIMEOUT}"
+helm install -f "https://raw.githubusercontent.com/ory/k8s/${BASE_RELEASE}/.circleci/values/$1.yaml" "${release}" "ory/$1" --wait --timeout="${TIMEOUT}"
 export INSTALLATION_STATUS=$?
 set -e
 
