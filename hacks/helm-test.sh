a3bfd6f1ac4287610d04f2ac721d5c143fc4be62
Alexandre Burgoni <burgoni.alexandre@protonmail.com>
2022-07-26 14:03:36 +0200

    ci: migrate to github action (#467) (#472)

---
+++ hacks/helm-test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+export TIMEOUT="180s"
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
+export release=$(echo "$1-$(date +%s)" | cut -c 1-31)
+
+helm dep update "./helm/charts/$1"
+
+echo "---> Installing $1"
+
+set +e
+helm install -f "hacks/values/$1.yaml" "${release}" "./helm/charts/$1" --wait --debug --atomic --timeout="${TIMEOUT}"
+export INSTALLATION_STATUS=$?
+set -e
+
+if [[ ${INSTALLATION_STATUS} -ne 0 ]]; then
+  echo "Installation of ${1} failed"
+  kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
+  kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
+  exit "${INSTALLATION_STATUS}"
+fi
+
+echo "---> Testing $1"
+
+n=0
+until [[ $n -ge 15 ]]; do
+  set +e
+  helm test --timeout "${TIMEOUT}" "${release}"
+  TEST_STATUS=$?
+  set -e
+
+  if [[ ${TEST_STATUS} -eq 0 ]]; then
+    echo "---> Test Successful"
+    exit ${TEST_STATUS}
+  fi
+  n=$(( n+1 ))
+  sleep 10
+done
+
+echo "---> Something failed along the way"
+exit 1
--- hacks/helm-validate.sh
+++ hacks/helm-validate.sh
@@ -6,4 +6,4 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 schema_url="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
 
-helm kubeval "./helm/charts/${1}" --exit-on-error --strict --schema-location "${schema_url}" -f ".circleci/values/${1}.yaml" --force-color
+helm kubeval "./helm/charts/${1}" --exit-on-error --strict --schema-location "${schema_url}" -f "hacks/values/${1}.yaml" --force-color
