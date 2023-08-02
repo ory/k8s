ab0bb62b4b6d5bb810f4f831732df41954708ca7
Jakub Błaszczyk <daemonsthere@gmail.com>
2023-08-02 12:03:43 +0200

    chore: migrate kubeval to kubeconform (#629)

--- hacks/helm-validate.sh
+++ hacks/helm-validate.sh
@@ -5,5 +5,6 @@ set -Eeuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 schema_url="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
+k8s_version="1.27.2"
 
-helm kubeval "./helm/charts/${1}" --exit-on-error --strict --schema-location "${schema_url}" -f "hacks/values/${1}.yaml" --force-color
+helm kubeconform "./helm/charts/${1}" --strict --schema-location "${schema_url}" -f "hacks/values/${1}.yaml" --kubernetes-version "${k8s_version}" --summary --verbose
