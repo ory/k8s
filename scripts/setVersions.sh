1ba703dff180651cb271b6ac698b519eca923661
Demonsthere <daemonsthere@gmail.com>
2025-02-28 17:54:35 +0100

    fix: rewrite release procedure for new yq

---
+++ scripts/setVersions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+progname=$(basename $0)
+
+VERSION="$1"
+
+charts=("example-idp" "hydra-maester" "hydra" "keto" "kratos" "kratos-selfservice-ui-node" "oathkeeper-maester" "oathkeeper")
+charts_with_maesters=("hydra" "oathkeeper")
+
+for chart in "${charts[@]}"
+do
+	yq -i ".version = ${VERSION}" "helm/charts/${chart}/Chart.yaml" || {
+    	echo "Failed to update version in 'helm/charts/${chart}/Chart.yaml'"
+        exit 1
+    }
+done
+
+for chart in "${charts_with_maesters[@]}"
+do
+	yq -i "(.dependencies[] | select(.name == \"${chart}-maester\")).version = ${VERSION}" "helm/charts/${chart}/Chart.yaml" || {
+    	echo "Failed to update version in 'helm/charts/${chart}/Chart.yaml'"
+        exit 1
+    }
+done
