ffddb277f2ffc3ad913bcaf0d65123e82a1f1287
Demonsthere <daemonsthere@gmail.com>
2025-02-28 18:10:18 +0100

    fix: syntax escape

--- scripts/setVersions.sh
+++ scripts/setVersions.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 progname=$(basename $0)
-
+set -x
 VERSION="$1"
 
 charts=("example-idp" "hydra-maester" "hydra" "keto" "kratos" "kratos-selfservice-ui-node" "oathkeeper-maester" "oathkeeper")
@@ -11,7 +11,7 @@ charts_with_maesters=("hydra" "oathkeeper")
 
 for chart in "${charts[@]}"
 do
-	yq -i ".version = ${VERSION}" "helm/charts/${chart}/Chart.yaml" || {
+	yq -i ".version = \"${VERSION}\"" "helm/charts/${chart}/Chart.yaml" || {
     	echo "Failed to update version in 'helm/charts/${chart}/Chart.yaml'"
         exit 1
     }
@@ -19,7 +19,7 @@ done
 
 for chart in "${charts_with_maesters[@]}"
 do
-	yq -i "(.dependencies[] | select(.name == \"${chart}-maester\")).version = ${VERSION}" "helm/charts/${chart}/Chart.yaml" || {
+	yq -i "(.dependencies[] | select(.name == \"${chart}-maester\")).version = \"${VERSION}\"" "helm/charts/${chart}/Chart.yaml" || {
     	echo "Failed to update version in 'helm/charts/${chart}/Chart.yaml'"
         exit 1
     }
