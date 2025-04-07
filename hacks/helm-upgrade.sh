e8826391f464f1547763d608a7d4992e70c5a392
Demonsthere <daemonsthere@gmail.com>
2025-04-07 15:44:47 +0200

    refactor upgrade tests

--- hacks/helm-upgrade.sh
+++ hacks/helm-upgrade.sh
@@ -1,63 +1,79 @@
 #!/bin/bash
 
 set -Eeuo pipefail
-export TIMEOUT="180s"
 
-export BASE_RELEASE=$(helm search repo "ory/$1" | awk '{print $2}' | head -2 | tail -1)
+export TIMEOUT="180s"
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-export release=$(echo "$1-$(date +%s)" | cut -c 1-31)
+CHART_NAME="${1}"
 
-if [[ "$1" == "ory-commons" ]]; then
+if [[ "${CHART_NAME}" == "ory-commons" ]]; then
   echo "---> Library chart, exitting"
   exit 0
 fi
 
-echo "---> Installing $1 from v${BASE_RELEASE}"
+helm dep update "./helm/charts/${CHART_NAME}"
 
-set +e
-helm install -f "https://raw.githubusercontent.com/ory/k8s/v${BASE_RELEASE}/hacks/values/$1.yaml" "${release}" "ory/$1" --wait --atomic --timeout="${TIMEOUT}"
-export INSTALLATION_STATUS=$?
-set -e
-
-if [[ ${INSTALLATION_STATUS} -ne 0 ]]; then
-  echo "Installation of ${1} failed"
-  kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
-  kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
-  exit "${INSTALLATION_STATUS}"
-fi
-
-echo "---> Upgrading $1"
-
-set +e
-helm upgrade -f "hacks/values/$1.yaml" "${release}" "./helm/charts/$1" --wait --debug --timeout="${TIMEOUT}"
-export UPGRADE_STATUS=$?
-set -e
-
-if [[ ${UPGRADE_STATUS} -ne 0 ]]; then
-  echo "Upgrade of ${1} failed"
-  kubectl describe pods -A -l "app.kubernetes.io/instance=${release}" || true
-  kubectl logs --all-containers=true -l "app.kubernetes.io/instance=${release}" || true
-  exit "${UPGRADE_STATUS}"
-fi
-
-echo "---> Testing $1"
-
-n=0
-until [[ $n -ge 15 ]]; do
+for val in $(ls "hacks/values/${CHART_NAME}")
+do
+  echo "---> Installing ${CHART_NAME}/${val}"
+  set -x
+  export release=$(echo "${CHART_NAME}-${val%%.*}-$(date +%s)" | cut -c 1-51)
   set +e
-  helm test --timeout "${TIMEOUT}" "${release}"
-  TEST_STATUS=$?
+  helm install \
+    -f "hacks/values/${CHART_NAME}/${val}" \
+    "${release}" "./helm/charts/${CHART_NAME}" \
+    --wait --debug --atomic --timeout="${TIMEOUT}"
+  
+  export INSTALLATION_STATUS=$?
   set -e
 
-  if [[ ${TEST_STATUS} -eq 0 ]]; then
-    echo "---> Test Successful"
-    exit ${TEST_STATUS}
+  if [[ ${INSTALLATION_STATUS} -ne 0 ]]; then
+    echo "Installation of ${CHART_NAME} failed"
+    kubectl describe pods \
+      -A -l "app.kubernetes.io/instance=${release}" || true
+    kubectl logs \
+      --all-containers=true \
+      -A -l "app.kubernetes.io/instance=${release}" || true
+    exit "${INSTALLATION_STATUS}"
   fi
-  n=$(( n+1 ))
-  sleep 10
-done
 
-echo "---> Something failed along the way"
-exit 1
+  echo "---> Upgrading ${CHART_NAME}/${val}"
+
+  set +e
+  helm upgrade \
+    -f "hacks/values/${CHART_NAME}/${val}" \
+    "${release}" "./helm/charts/$1" \
+    --wait --debug --timeout="${TIMEOUT}"
+  export UPGRADE_STATUS=$?
+  set -e
+
+  if [[ ${UPGRADE_STATUS} -ne 0 ]]; then
+    echo "Upgrade of ${CHART_NAME}/${val} failed"
+    kubectl describe pods \
+      -A -l "app.kubernetes.io/instance=${release}" || true
+    kubectl logs \
+      --all-containers=true \
+      -A -l "app.kubernetes.io/instance=${release}" || true
+    exit "${UPGRADE_STATUS}"
+  fi
+
+  echo "---> Testing ${CHART_NAME}/${val}"
+
+  n=0
+  until [[ $n -ge 15 ]]; do
+    set +e
+    helm test \
+      --timeout "${TIMEOUT}" "${release}"
+    TEST_STATUS=$?
+    set -e
+
+    if [[ ${TEST_STATUS} -eq 0 ]]; then
+      echo "---> Test Successful"
+      break
+    fi
+    n=$(( n+1 ))
+    sleep 10
+  done
+done
